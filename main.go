b4c274df5e90393f7dacc54f7b06a1e9ceea11ea
Siddharth Prasad <sidsprasad@users.noreply.github.com>
2024-01-23 17:40:52 +0900

    Add more logging for retrieving author from commit

--- main.go
+++ main.go
@@ -353,8 +353,11 @@ func (s *service) assignNewIssues() {
 }
 
 func (s *service) retrieveCommitAuthor(commitHash string, title string) {
+	log.Printf("Retrieving author for commit '%v'...", commitHash)
+
 	// First check cache to see if this commit was already retrieved before
 	if commitAuthor, ok := s.commitToAuthorCache[commitHash]; ok {
+		log.Printf("Cache hit. Returning author='%v' for commit '%v'", commitAuthor, commitHash)
 		s.issueTitleToAssigneeMap[title] = commitAuthor
 		return
 	}
